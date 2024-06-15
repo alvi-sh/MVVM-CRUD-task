bfa4f218f5c3ea058628e261876a0486d179e932
alvi-sh <alvishstha97@gmail.com>
2024-06-15 13:45:17 +0545

    login ui

---
+++ app/src/main/java/com/example/mvvmcrudtask/ui/activity/SplashScreen.kt
@@ -0,0 +1,31 @@
+package com.example.mvvmcrudtask.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mvvmcrudtask.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this@SplashScreen, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
