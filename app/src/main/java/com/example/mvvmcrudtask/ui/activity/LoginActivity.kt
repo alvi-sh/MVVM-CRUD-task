9f458c69fa3ccda2be8fc4f69d5d11a0790bd101
alvi-sh <alvishstha97@gmail.com>
2024-06-15 15:16:43 +0545

    authentication of login and signup done

--- app/src/main/java/com/example/mvvmcrudtask/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/mvvmcrudtask/ui/activity/LoginActivity.kt
@@ -3,17 +3,20 @@ package com.example.mvvmcrudtask.ui.activity
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mvvmcrudtask.R
 import com.example.mvvmcrudtask.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 import java.util.prefs.Preferences
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var loginBinding: ActivityLoginBinding
+    var auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +25,25 @@ class LoginActivity : AppCompatActivity() {
         loginBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(loginBinding.root)
 
+        loginBinding.loginButton.setOnClickListener {
+            var email = loginBinding.loginEmail.text.toString()
+            var password = loginBinding.loginPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Login Successful", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(applicationContext, task.exception?.message.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
         loginBinding.gotoSignup.setOnClickListener {
-            val intent= Intent(this@LoginActivity, SignUpActivity::class.java)
+            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
             startActivity(intent)
             finish()
         }
--- app/src/main/java/com/example/mvvmcrudtask/ui/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/mvvmcrudtask/ui/activity/SignUpActivity.kt
@@ -2,16 +2,19 @@ package com.example.mvvmcrudtask.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mvvmcrudtask.R
 import com.example.mvvmcrudtask.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
 
     lateinit var signupBinding: ActivitySignUpBinding
+    var auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,30 @@ class SignUpActivity : AppCompatActivity() {
 
         signupBinding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(signupBinding.root)
+
+        signupBinding.signupButton.setOnClickListener {
+            val username = signupBinding.signupUsername.text.toString()
+            val email = signupBinding.signupEmail.text.toString()
+            val password = signupBinding.signupPassword.text.toString()
+            val confirmPassword = signupBinding.signupConfirmPassword.text.toString()
+
+            if (username.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(applicationContext, "All fields must be filled", Toast.LENGTH_LONG).show()
+            } else if (password == confirmPassword) {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(applicationContext, "Account Created", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(applicationContext, task.exception?.message.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+            } else {
+                Toast.makeText(applicationContext, "Passwords don't match", Toast.LENGTH_LONG).show()
+            }
+        }
 
         signupBinding.gotoLogin.setOnClickListener {
             val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
